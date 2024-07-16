b6fffb034fd45e3b0c63d8c6b082a99dce9f2018
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-16 14:22:09 +0200

    fix: try/catch added to authenticationToken func

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,18 +2,22 @@ import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
 
 export function authenticateToken(req: Request, res: Response, next: NextFunction): void {
-    const token = req.header('Authorization')?.split(' ')[1];
-    if (!token) {
-        res.sendStatus(401);
-        return;
-    }
-
-    jwt.verify(token!, process.env.TOKEN_SECRET!, (err: any) => {
-        if (err) {
-            res.sendStatus(403);
+    try {
+        const token = req.header('Authorization')?.split(' ')[1];
+        if (!token) {
+            res.sendStatus(401);
             return;
         }
 
-        next();
-    });
+        jwt.verify(token!, process.env.TOKEN_SECRET!, (err: any) => {
+            if (err) {
+                res.sendStatus(403);
+                return;
+            }
+
+            next();
+        });
+    } catch(error) {
+        res.sendStatus(500);
+    }
 }
