bb30321f0e16f008c6ddc4d2c0c024b0c6c08820
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-16 13:52:35 +0200

    fix: name modification of room to roomsController

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import bookingController from './controllers/bookingControllers';
 import 'dotenv/config';
 import loginController from './controllers/loginController';
-import roomController from './controllers/roomControlers';
+import roomsController from './controllers/roomsControlers';
 
 process.env.TOKEN_SECRET;
 
@@ -12,7 +12,7 @@ const port = 3000;
 app.use(express.json());
 app.use('/login', loginController);
 app.use('/bookings', bookingController);
-app.use('/rooms', roomController);
+app.use('/rooms', roomsController);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
