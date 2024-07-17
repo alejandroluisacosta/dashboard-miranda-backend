75f4d7eec8c9a37f344fff7837cd0ca8b17bb283
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-17 12:38:15 +0200

    add: public route

--- index.ts
+++ index.ts
@@ -1,15 +1,25 @@
 import express from 'express';
+import path from 'path';
 import bookingController from './controllers/bookingControllers';
 import 'dotenv/config';
 import loginController from './controllers/loginController';
 import roomsController from './controllers/roomsControllers';
 import usersController from './controllers/usersControllers';
 import commentController from './controllers/commentControllers';
+import mustacheExpress from 'mustache-express'
 
 process.env.TOKEN_SECRET;
 
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+app.get('/public', (_req, res) => {
+    res.render('index', { name: "Mr. Pebbles" });
+  })
 
 app.use(express.json());
 app.use('/login', loginController);
