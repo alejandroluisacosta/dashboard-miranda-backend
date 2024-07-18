a2808745141ce18acff6a871bafe429a38a95e96
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-18 13:07:10 +0200

    add: bookings controllers authentication tests

--- tests/bookings.test.ts
+++ tests/bookings.test.ts
@@ -6,7 +6,26 @@ const { app }  = require('../index');
 
 describe('Bookings controller tests', () => {
   let token: string;
+  
+  it('Authentication middleware denies requests of users that aren\'t logged in', async () => {
+    const res = await request(app)
+      .get('/bookings');
 
+    expect(res.status).toBe(401);
+  })
+
+  it('Authentication middleware denies requests if credentials aren\'t correct', async () => {
+    const res = await request(app)
+      .post('/login')
+      .send({
+          "username": "Johnny",
+          "password": "12345"
+      })
+      .set("Content-Type", "application/json");
+
+      expect(res.status).toBe(401);
+  })
+  
   beforeEach(async () => {
     const authRes = await request(app)
       .post('/login')
