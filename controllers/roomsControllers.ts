f721515212fc2d4efb821a758dc0adcd36cc5204
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-18 12:13:36 +0200

    fix: try/catchs added to rooms controllers

--- controllers/roomsControllers.ts
+++ controllers/roomsControllers.ts
@@ -1,36 +1,56 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { RoomModel } from '../services/roomServices';
 import Room from '../interfaces/room';
 
 const roomsController = express.Router();
 
-roomsController.get('/', (_req: Request, res: Response): Response<JSON> => {
-    const rooms: Room[] = RoomModel.getRooms();
-    return res.json(rooms);
+roomsController.get('/', (_req: Request, res: Response, next: NextFunction): Response<JSON> | void=> {
+    try {
+        const rooms: Room[] = RoomModel.getRooms();
+        return res.json(rooms);
+    } catch (error) {
+        next(error);
+    }
 })
 
-roomsController.get('/:id', (req: Request, res: Response): Response<JSON> => {
-    const id: string = req.params.id;
-    const room = RoomModel.getRoom(id);
-    return res.json(room);
+roomsController.get('/:id', (req: Request, res: Response, next: NextFunction): Response<JSON> | void=> {
+    try {
+        const id: string = req.params.id;
+        const room = RoomModel.getRoom(id);
+        return res.json(room);
+    } catch (error) {
+        next(error);
+    }
 })
 
-roomsController.post('/', (req: Request, res: Response): Response<JSON> => {
-    const newRoom: Room = req.body as Room;
-    RoomModel.addRoom(newRoom);
-    return res.json(newRoom);
+roomsController.post('/', (req: Request, res: Response, next: NextFunction): Response<JSON> | void=> {
+    try {
+        const newRoom: Room = req.body as Room;
+        RoomModel.addRoom(newRoom);
+        return res.json(newRoom);
+    } catch (error) {
+        next(error);
+    }
 })
 
-roomsController.delete('/', (req: Request, res: Response): Response<JSON> => {
-    const id: string = req.body.id;
-    const updatedRooms: Room[] = RoomModel.removeRoom(id);
-    return res.json(updatedRooms);
+roomsController.delete('/', (req: Request, res: Response, next: NextFunction): Response<JSON> | void=> {
+    try {
+        const id: string = req.body.id;
+        const updatedRooms: Room[] = RoomModel.removeRoom(id);
+        return res.json(updatedRooms);
+    } catch (error) {
+        next(error);
+    }
 })
 
-roomsController.put('/', (req: Request, res: Response): Response<JSON> => {
-    const modifiedRoom: Room = req.body;
-    const updatedRooms: Room[] = RoomModel.modifyRoom(modifiedRoom);
-    return res.json(updatedRooms);
+roomsController.put('/', (req: Request, res: Response, next: NextFunction): Response<JSON> | void=> {
+    try {
+        const modifiedRoom: Room = req.body;
+        const updatedRooms: Room[] = RoomModel.modifyRoom(modifiedRoom);
+        return res.json(updatedRooms);
+    } catch (error) {
+        next(error);
+    }
 })
 
 export default roomsController;
