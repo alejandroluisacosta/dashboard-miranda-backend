eb44405ecc356437b8f132b67269e6b8dfb01059
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-19 12:29:28 +0200

    "remove: private folder"

--- dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
-index_1.app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
