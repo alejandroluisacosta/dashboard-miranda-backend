edff15769a5b34a222fb251ac58a9d8b3398b145
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-22 13:57:38 +0200

    add: database initialization logic

---
+++ db.ts
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+connectDB().catch(err => console.log(err));
--- server.ts
+++ server.ts
@@ -1,7 +1,18 @@
 import { app } from './app'
+import { connectDB } from './db';
 
 const port = process.env.PORT ?? 3000;
 
-app.listen(port, () =>
-	console.log(`Server listening on http://localhost:${port}`)
-)
+async function startServer() {
+	try {
+		await connectDB();
+		app.listen(port, () =>
+			console.log(`Server listening on http://localhost:${port}`)
+		);
+
+	} catch (err) {
+		console.error('Unexpected error occurred', err);
+	}
+}
+
+startServer();
