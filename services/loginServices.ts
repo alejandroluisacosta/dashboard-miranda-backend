4a7311d719f0e019412bcb19a3a00afd153715cd
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-23 13:41:17 +0200

    add: login services encrypted password check

--- services/loginServices.ts
+++ services/loginServices.ts
@@ -1,21 +1,20 @@
 import User from '../interfaces/User';
 import UserModel from '../models/User';
 import { generateAccessToken } from '../utils/authUtils';
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 
 class LoginServices {
 
     static async authenticateUser(user: User): Promise<string> {
         
         const userToCheck: User | null = await UserModel.findOne({ userName: user.userName });
-        console.log(userToCheck)
         
         if (userToCheck && user.userName === userToCheck.userName) {
-            // const match = await bcrypt.compare(user.password, userToCheck.password);
-            // if (match) {
-                // }
+            const match = await bcrypt.compare(user.password, userToCheck.password);
+            if (match) {
                 const token = generateAccessToken(user.userName);
                 return token;
+                }
         }
         throw new Error('Invalid credentials');
     }
